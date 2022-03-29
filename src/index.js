e4cf7d484fffda8e55e794a0d8e3afbf32316585
thunderheina <rheinadlon@gmail.com>
2022-03-29 21:22:04 +0700

    hashrouter

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HasRouter>
       <App />
-    </BrowserRouter>
+    </HasRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
