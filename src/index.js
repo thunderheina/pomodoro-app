747e415dd2ab5bcbcfc08202c66dab39dfc6d80d
thunderheina <rheinadlon@gmail.com>
2022-03-29 21:24:19 +0700

    hashrouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HasRouter>
+    <HashRouter>
       <App />
-    </HasRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
